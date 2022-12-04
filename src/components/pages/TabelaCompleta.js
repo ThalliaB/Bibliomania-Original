d5b09e944ae410f558c3d30886da3a2a40fc00d9
ThalliaB <thalliabrum60@gmail.com>
2022-12-03 22:23:50 -0300

    Add some database modifications

--- src/components/pages/TabelaCompleta.js
+++ src/components/pages/TabelaCompleta.js
@@ -2,12 +2,14 @@ import Table2019 from '../project/Table2019/Table2019'
 import Table2020 from '../project/Table2020/Table2020'
 import Table2021 from '../project/Table2021/Table2021'
 import Table2022 from '../project/Table2022/Table2022'
+// import Table2023 from '../project/Table2023/Table2023'
 
 function TabelaCompleta() {
     
     return (
         <>
-            <Table2022/>
+            {/* <Table2023 /> */}
+            <Table2022 />
             <Table2021 />
             <Table2020 />
             <Table2019 />
